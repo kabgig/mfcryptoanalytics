976da86470561efb0fd5a001c49df1860eb0778e
Ansar Ismagilov <a-ismagilov@marya.ru>
2026-04-23 14:37:32 +0700

    fix: add type annotation for networks export in config.ts

--- lib/reown/config.ts
+++ lib/reown/config.ts
@@ -1,6 +1,7 @@
 import { cookieStorage, createStorage } from '@wagmi/core'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
 import { mainnet, arbitrum, polygon } from '@reown/appkit/networks'
+import type { AppKitNetwork } from '@reown/appkit/networks'
 
 export const projectId = process.env.NEXT_PUBLIC_REOWN_PROJECT_ID
 
@@ -8,7 +9,7 @@ if (!projectId) {
   throw new Error('NEXT_PUBLIC_REOWN_PROJECT_ID is not defined')
 }
 
-export const networks = [mainnet, arbitrum, polygon] as const
+export const networks: [AppKitNetwork, ...AppKitNetwork[]] = [mainnet, arbitrum, polygon]
 
 export const wagmiAdapter = new WagmiAdapter({
   storage: createStorage({ storage: cookieStorage }),
